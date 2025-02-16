841a8f2860af6a9ebedcf9c6972c8e509281d1bb
A8tonDerCoole <a8ton.istcool2009@outlook.de>
2025-02-16 11:12:21 +0100

    Added fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Eight Services - A8ton'
+description 'Exclusionzone'
+version '1.0.0'
+
+shared_scripts { 'config.lua', 'locales/locale.lua' }
+
+server_script 'server/*.lua'
+
+client_script 'client/client.lua'
